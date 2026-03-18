2bbbc5dccf4eb2fc5173e487cb80511e3bff11dc
Ananya Awasthi <aananya255@gmail.com>
2026-03-19 00:13:17 +0530

    voice added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
-from app.routes import resume , interview
+from app.routes import resume , interview , confidence
+import threading
+from app.services.ai_engine import start_confidence_tracking
 from fastapi.middleware.cors import CORSMiddleware
+
+threading.Thread(target=start_confidence_tracking, daemon=True).start()
 
 app = FastAPI()
 # ✅ ADD THIS BLOCK
@@ -14,6 +18,7 @@ app.add_middleware(
 
 app.include_router(resume.router)
 app.include_router(interview.router)
+app.include_router(confidence.router)
 
 @app.get("/")
 def home():
