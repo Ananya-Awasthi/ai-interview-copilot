b505a62a788c2cdc133dc03abf3095f497a4760b
Samridh Pathak <samridhpathak007@gmail.com>
2026-03-18 23:04:27 +0530

    Added OpenCV confidence tracking + proctoring system

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
-from app.routes import resume , interview
+from app.routes import resume, interview, confidence
+import threading
+from app.services.vision import start_confidence_tracking
+
+threading.Thread(target=start_confidence_tracking, daemon=True).start()
 
 app = FastAPI()
 
 app.include_router(resume.router)
 app.include_router(interview.router)
+app.include_router(confidence.router)
 
 @app.get("/")
 def home():
